951acd6025bdd056bc2dfc5655b4057fa16ca947
QUOC-KHANH\saakh <saakhaka@gmail.com>
2019-01-17 17:22:53 +0700

    Main View Interface

---
+++ app/src/main/java/khanh/mvpandroid/View/Interface/MainView.java
@@ -0,0 +1,9 @@
+package khanh.mvpandroid.View.Interface;
+
+import java.util.List;
+
+import khanh.mvpandroid.Model.Entity.Demo;
+
+public interface MainView {
+    void displayMain(List<Demo> listDemo);
+}
