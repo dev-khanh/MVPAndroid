8fc97ba8448344f1f9a84010424977db5312b38c
QUOC-KHANH\saakh <saakhaka@gmail.com>
2019-01-17 18:36:42 +0700

    upload activity main

--- app/src/main/java/khanh/mvpandroid/MainActivity.java
+++ app/src/main/java/khanh/mvpandroid/MainActivity.java
@@ -1,15 +1,31 @@
 package khanh.mvpandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
 
+import khanh.mvpandroid.Model.Entity.Demo;
+import khanh.mvpandroid.Presenter.MainPresenter;
+import khanh.mvpandroid.View.Interface.MainView;
 
+public class MainActivity extends AppCompatActivity implements MainView {
+
+    private MainPresenter mainPresenter;
+    private ListView listView;
+    private Button btnLoand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void displayMain(List<Demo> listDemo) {
+
     }
 }
