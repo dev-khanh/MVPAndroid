261ea9b8efaec5187fd9cd7c59efd0ce34c523ba
QUOC-KHANH\saakh <saakhaka@gmail.com>
2019-01-17 18:29:47 +0700

    Them interface

--- app/src/main/java/khanh/mvpandroid/Model/Interface/LoadDemoListener.java
+++ app/src/main/java/khanh/mvpandroid/Model/Interface/LoadDemoListener.java
@@ -1,4 +1,10 @@
 package khanh.mvpandroid.Model.Interface;
 
+import java.util.List;
+
+import khanh.mvpandroid.Model.Entity.Demo;
+
 public interface LoadDemoListener {
+    void onLoadDemoSuccess(List<Demo> listDemo);
+    void onLoadDemoFailure(String message);
 }
