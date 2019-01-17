b9928790bdfe9ca5c85815fddb00d976e2388119
QUOC-KHANH\saakh <saakhaka@gmail.com>
2019-01-17 17:32:10 +0700

    xử lý các logic, nhận dữ liệu từ lớp model thông qua các hàm callback, nhiệm vụ đẩy lên View hướng dẫn cách View hiển thị:

---
+++ app/src/main/java/khanh/mvpandroid/Presenter/MainPresenter.java
@@ -0,0 +1,33 @@
+package khanh.mvpandroid.Presenter;
+
+import java.util.List;
+
+import khanh.mvpandroid.Model.Entity.Demo;
+import khanh.mvpandroid.Model.Interface.LoadDemoListener;
+import khanh.mvpandroid.Model.UserInterator;
+import khanh.mvpandroid.View.Interface.MainView;
+
+public class MainPresenter implements LoadDemoListener {
+
+    private UserInterator mainInterator;
+    private MainView mainView;
+
+    public MainPresenter(UserInterator mainInterator, MainView mainView) {
+        this.mainInterator = mainInterator;
+        this.mainView = mainView;
+    }
+
+    public void loadData(){
+        mainInterator.createListData();
+    }
+
+    @Override
+    public void onLoandDemoSuccec(List<Demo> listDemo) {
+        mainView.displayMain(listDemo);
+    }
+
+    @Override
+    public void onLoandDemoFailure(String message) {
+
+    }
+}
