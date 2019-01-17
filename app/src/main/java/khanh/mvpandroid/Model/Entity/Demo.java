2bb900262133488c0e314b0f90c8c28a0715c9a0
QUOC-KHANH\saakh <saakhaka@gmail.com>
2019-01-17 18:27:53 +0700

    Them Model

--- app/src/main/java/khanh/mvpandroid/Model/Entity/Demo.java
+++ app/src/main/java/khanh/mvpandroid/Model/Entity/Demo.java
@@ -1,5 +1,22 @@
 package khanh.mvpandroid.Model.Entity;
 
 public class Demo {
+    private String message;
 
+    public Demo(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return  message;
+    }
 }
